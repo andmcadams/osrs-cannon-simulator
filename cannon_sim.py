f6290714680f087059252144f0d5b6cb350dcad3
Andrew McAdams <andmcadams@gmail.com>
2023-12-28 10:33:10 -0800

    Get cannon turning and firing to work

--- cannon_sim.py
+++ cannon_sim.py
@@ -1,7 +1,37 @@
+import math
 import random
+from collections import defaultdict
 
 players = []
 npc = []
+
+def cheb(point1, point2):
+  return max(abs(point1[0] - point2[0]), abs(point1[1] - point2[1]))
+
+def euclidean(point1, point2):
+  return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+npc_chunk_lookup = defaultdict(lambda: defaultdict(dict))
+def get_npcs_in_chunk(chunk_x, chunk_y):
+  return npc_chunk_lookup[chunk_x][chunk_y].values()
+
+def add_to_chunk(npc, chunk_x, chunk_y):
+  npc_chunk_lookup[chunk_x][chunk_y][npc.slot_index] = npc
+
+def remove_from_chunk(npc, chunk_x, chunk_y):
+  npc_chunk_lookup[chunk_x][chunk_y].pop(npc.slot_index, None)
+
+def get_chunk(x, y):
+  return (x // 8, y // 8)
+
+def next_slot_func():
+  count = 0
+  def inner():
+    nonlocal count
+    count += 1
+    return count
+  return inner
+next_slot = next_slot_func()
 
 
 class Action:
@@ -10,9 +40,14 @@ class Action:
 
 class Npc:
 
-  def __init__(self, hitpoints: int):
+  def __init__(self, x: int, y: int, hitpoints: int):
     self.hitpoints = hitpoints
     self.queue = []
+    self.slot_index = next_slot() # This should eventually be passed in
+    self._x = x
+    self._y = y
+    chunk = get_chunk(x, y)
+    add_to_chunk(self, chunk[0], chunk[1])
 
   def is_dead(self):
     return self.hitpoints <= 0
@@ -46,6 +81,24 @@ class Npc:
     # TODO: Target the player
     pass
 
+  def x(self):
+    return self._x
+
+  def y(self):
+    return self._y
+
+  def coordinate(self, x, y):
+    old_chunk = get_chunk(self._x, self._y)
+    new_chunk = get_chunk(x, y)
+    if new_chunk != old_chunk:
+      remove_from_chunk(self, old_chunk[0], old_chunk[1])
+      add_to_chunk(self, new_chunk[0], new_chunk[1])
+    self._x = x
+    self._y = y
+
+  def coordinate(self):
+    return (self.x(), self.y())
+
 class DamageAction(Action):
   def __init__(self, damage):
     self.damage = damage
@@ -63,27 +116,54 @@ class Cannon:
 
     self.MOVEMENTS = {
       0: {1: (1, 1), -1: (-1, -1)},
-      1: {1: (1, 0), -1: (0, -1)},
-      -1: {1: (0, 1), -1: (-1, 0)},
+      1: {1: (1, 0), 0: (1, -1), -1: (0, -1)},
+      -1: {-1: (-1, 0), 0: (-1, 1), 1: (0, 1)},
     }
 
   def process_tick(self):
-    print('Processing cannon tick')
     self.fire()
     self.turn()
 
   def turn(self):
     # Definitely some way to not hardcode but I can't be bothered
-    self.direction = self.MOVEMENTS[self._x][self._y]
+    self.direction = self.MOVEMENTS[self.direction[0]][self.direction[1]]
 
   def fire(self):
-    # TODO: Somehow pick an npc
-    npc = npcs[0]
-    self.queue_damage(npc)
+    npc = self.get_target()
+    if npc:
+      self.queue_damage(npc)
 
   def queue_damage(self, npc: Npc):
     damage = random.randint(0, 25)
     npc.add_to_queue(DamageAction(damage))
+  
+  def get_target(self):
+    origin = (self._x, self._y)
+    direction = self.direction
+    ordinal_cannon_distances = [2, 5, 12]
+    cardinal_cannon_distances = [3, 7, 14]
+    cannon_ranges = [1, 2, 5]
+
+    is_cardinal = (direction[0] + direction[1]) % 2 != 0
+    distances = cardinal_cannon_distances if is_cardinal else ordinal_cannon_distances
+    for i in range(3):
+        center = (origin[0] + direction[0] * distances[i], origin[1] + direction[1] * distances[i])
+        center_chunk = (center[0]//8, center[1]//8)
+
+        npcs_in_range = []
+        for x_chunk_offset in [1, 0, -1]:
+            for y_chunk_offset in [1, 0, -1]:
+                chunk = (center_chunk[0] + x_chunk_offset, center_chunk[1] + y_chunk_offset)
+                for npc in get_npcs_in_chunk(chunk[0], chunk[1]):
+                    if cheb(center, npc.coordinate()) <= cannon_ranges[i]:
+                        npcs_in_range.append(npc)
+
+        npcs_in_range.sort(key=lambda x: euclidean(center, npc.coordinate()))
+
+        if len(npcs_in_range) > 0:
+            return npcs_in_range[0]
+
+    return None
 
 class Player:
   def __init__(self):
@@ -99,13 +179,12 @@ class Player:
   def cannon(self):
     return self._cannon
 
-npcs = [Npc(100)]
+npcs = [Npc(2, 0, 100)]
 player = Player()
-player.place_cannon(1, 1)
+player.place_cannon(0, 0)
 players = [player]
 def perform_tick():
   # Process client input
-
   for npc in npcs:
     # Each npc do
     #   stalls end
@@ -130,8 +209,10 @@ def perform_tick():
     #   * (not v0) movement
     #   * (not v0) interaction with players/npcs
 
+tick_num = 0
 while True:
+  print(f'{tick_num}')
   perform_tick()
-  print(f'{npcs[0].hitpoints}')
   if npcs[0].is_dead():
     break
+  tick_num += 1
